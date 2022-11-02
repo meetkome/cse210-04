b541f4ba5665eb959539b9cf7e81caa2d134870d
yaqman <kokiyaq57@byui.edu>
2022-11-02 22:24:38 +0900

    add parent class

--- greed/falling_object.py
+++ greed/falling_object.py
@@ -1,5 +1,6 @@
 import random
 from point import Point
+from color import Color
 
 class FallingObject:
     def __init__(self):
@@ -7,6 +8,8 @@ class FallingObject:
         self.position = Point(random.randint(2, 890), 0)
         self.points = 1
         self.move_counter = 0
+        self._color = Color(255, 255, 255)
+        self._velocity = Point(0, 0)
         
     def fall(self):
         if self.move_counter < 30:
@@ -14,3 +17,4 @@ class FallingObject:
         else:
             self.position.y += 1
             self.move_counter = 0
+    
